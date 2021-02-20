cb6ea655f1c23f8c3448141a77e0dcda6978dd8e
Askat-makers <askatdovut1@gmail.com>
2021-02-21 00:51:53 +0600

    change JSON API and script START

--- src/helpers/Constants.jsx
+++ src/helpers/Constants.jsx
@@ -1,3 +1,3 @@
-export const JSON_API = 'http://localhost:8000'
+export const JSON_API = 'https://bmx-kg.herokuapp.com/api'
 
 // export const JSON_API = 'https://rider-kg-default-rtdb.firebaseio.com/'
